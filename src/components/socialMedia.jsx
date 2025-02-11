7f8fcb4c4e2ef2b5b204c5cae69f56313c55bffe
AnuragPrajapati15 <anurag.prajapatiktn@gmail.com>
2025-02-12 01:57:42 +0530

    social media links

--- src/components/socialMedia.jsx
+++ src/components/socialMedia.jsx
@@ -1,21 +1,24 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
+import React from "react";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 const SocialMediaLinks = () => {
   return (
-    <div className="d-flex justify-content-center gap-3 mt-4 ">
-      <a href="https://www.instagram.com/yourusername" target="_blank" rel="noopener noreferrer" className="social-icon text-light">
-        <i className="fab fa-instagram fs-3"></i>
-      </a>
-      <a href="https://www.linkedin.com/in/yourusername" target="_blank" rel="noopener noreferrer" className="social-icon text-light">
-        <i className="fab fa-linkedin fs-3"></i>
-      </a>
-      <a href="https://github.com/yourusername" target="_blank" rel="noopener noreferrer" className="social-icon text-light">
-        <i className="fab fa-github fs-3"></i>
-      </a>
-      <a href="https://x.com/yourusername" target="_blank" rel="noopener noreferrer" className="social-icon text-light">
-        <i className="fab fa-x fs-3"></i>
-      </a>
+    <div className="container text-center mt-4">
+      <h5>Follow Me</h5>
+      <div className="d-flex justify-content-center gap-3">
+        <a href="https://www.instagram.com/anurag_prajapati__" target="_blank" rel="noopener noreferrer" className=" text-danger">
+          <i className="bi bi-instagram fs-3"></i>
+        </a>
+        <a href="https://www.linkedin.com/in/anurag-prajapati-25ab8a20b" target="_blank" rel="noopener noreferrer" className=" text-primary">
+          <i className="bi bi-linkedin fs-3"></i>
+        </a>
+        <a href="https://github.com/AnuragPrajapati15" target="_blank" rel="noopener noreferrer" className="text-dark">
+          <i className="bi bi-github fs-3"></i>
+        </a>
+        <a href="https://x.com/anuragpraj68033" target="_blank" rel="noopener noreferrer" className=" text-info">
+          <i className="bi bi-twitter-x fs-3"></i>
+        </a>
+      </div>
     </div>
   );
 };
